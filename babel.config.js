94182e03974901489167b04118acfafd5ace7ea2
Heltonss <Helton@heltonsouza.com.br>
2019-05-27 08:09:47 -0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathSuffix: 'src',
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: [
+          [
+            'babel-plugin-root-import',
+            {
+              rootPathSuffix: 'src',
+            },
+          ],
+        ],
+      },
+    },
+  };
+};
